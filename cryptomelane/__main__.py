1f3f1bbc6a052b7e5256a71526be924da6f32607
A_D <aunderscored@gmail.com>
2021-10-04 03:42:59 +0200

    use the right file

--- cryptomelane/__main__.py
+++ cryptomelane/__main__.py
@@ -1,3 +1,4 @@
+import sys
 import asyncio
 import signal
 
@@ -6,19 +7,15 @@ import toml
 from .bot import Cryptomelane, BotConfig
 
 if __name__ == '__main__':
-    conf = BotConfig.from_dict(toml.load('./config.example.toml'))
+    conf = BotConfig.from_dict(toml.load('./config.toml'))
     print(conf)
     b = Cryptomelane(conf)
     loop = asyncio.get_event_loop()
 
     def on_sig():
         print("Caught SIGINT")
-        loop.create_task(b.stop())
+        sys.exit(0)
 
     loop.add_signal_handler(signal.SIGINT, on_sig)
 
-    async def testing_things():
-        print('testing')
-        await b.testing_things()
-
-    loop.run_until_complete(testing_things())
+    loop.run_until_complete(b.run())
